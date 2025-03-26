977f6cbc4e6e8c15f29c8b489e11f603175a1965
breismanC <breischala9612@gmail.com>
2025-03-25 23:00:36 -0500

    Add Slice Machine configuration and API routes for integration with Prismic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import clsx from "clsx";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const nunito = Nunito({
   subsets: ["latin"],
+  variable: "--font-nunito",
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={clsx((nunito.variable, nunitoSans.variable))}>
         {children}
       </body>
     </html>
